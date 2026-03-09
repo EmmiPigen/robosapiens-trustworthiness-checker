2a6ecff61373e45186aabf83fb8604dc2c067590
Emilie Bang Holmberg <emilie.b.holmberg@hotmail.dk>
2026-03-09 11:08:02 +0100

    Trying to fix the lack of errors even with syntax mistakes

--- src/lang/dsrv/parser.rs
+++ src/lang/dsrv/parser.rs
@@ -1089,15 +1089,15 @@ pub(crate) fn type_annotation(s: &mut &str) -> Result<StreamType> {
 }
 
 pub(crate) fn input_decl(s: &mut &str) -> Result<(VarName, Option<StreamType>)> {
-    seq!((
+    seq!(
         _: whitespace,
         _: literal("in"),
         _: loop_ms_or_lb_or_lc,
         ident,
         opt(type_annotation),
         _: whitespace,
-    ))
-    .map(|(name, typ): (&str, _)| (name.into(), typ))
+    )
+    .map(|(name, typ): (&str, Option<StreamType>)| (VarName::from(name), typ))
     .parse_next(s)
 }
 
@@ -1106,15 +1106,15 @@ pub(crate) fn input_decls(s: &mut &str) -> Result<Vec<(VarName, Option<StreamTyp
 }
 
 pub(crate) fn output_decl(s: &mut &str) -> Result<(VarName, Option<StreamType>)> {
-    seq!((
+    seq!(
         _: whitespace,
         _: literal("out"),
         _: loop_ms_or_lb_or_lc,
         ident,
         opt(type_annotation),
         _: whitespace,
-    ))
-    .map(|(name, typ): (&str, _)| (name.into(), typ))
+    )
+    .map(|(name, typ): (&str, Option<StreamType>)| (VarName::from(name), typ))
     .parse_next(s)
 }
 
@@ -1123,15 +1123,15 @@ pub(crate) fn output_decls(s: &mut &str) -> Result<Vec<(VarName, Option<StreamTy
 }
 
 pub(crate) fn aux_decl(s: &mut &str) -> Result<(VarName, Option<StreamType>)> {
-    seq!((
+seq!(
         _: whitespace,
         _: alt(("var", "aux")),
         _: loop_ms_or_lb_or_lc,
         ident,
         opt(type_annotation),
         _: whitespace,
-    ))
-    .map(|(name, typ): (&str, _)| (name.into(), typ))
+    )
+    .map(|(name, typ): (&str, Option<StreamType>)| (VarName::from(name), typ))
     .parse_next(s)
 }
 
@@ -1194,17 +1194,20 @@ pub fn dsrv_specification(s: &mut &str) -> Result<DsrvSpecification> {
 
 // New for not losing source info in assignment declarations in the LOLA specification
 pub fn lola_specification_with_source(source: &str, s: &mut &str) -> Result<LOLASpecification> {
-    seq!((
-        _: loop_ms_or_lb_or_lc,
-        input_decls,
-        _: loop_ms_or_lb_or_lc,
-        output_decls,
-        _: loop_ms_or_lb_or_lc,
-        aux_decls,
-        _: loop_ms_or_lb_or_lc,
-        |i: &mut &str | assignment_decls_with_source(source, i),
-        _: loop_ms_or_lb_or_lc,
-    ))
+    terminated(
+        seq!((
+            _: loop_ms_or_lb_or_lc,
+            input_decls,
+            _: loop_ms_or_lb_or_lc,
+            output_decls,
+            _: loop_ms_or_lb_or_lc,
+            aux_decls,
+            _: loop_ms_or_lb_or_lc,
+            |i: &mut &str | assignment_decls_with_source(source, i),
+            _: loop_ms_or_lb_or_lc,
+        )),
+        eof,
+    )
     .map(|(input_vars, output_vars, aux_vars, exprs)| {
         DsrvSpecification::new(
             input_vars.iter().map(|(name, _)| name.clone()).collect(),
